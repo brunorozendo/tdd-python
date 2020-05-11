7fee7983afa505bf7b9207d3022e6264f802a445
Bruno Rozendo <bruno@brunorozendo.com>
2020-05-11 15:43:04 -0300

    init test

---
+++ test/functional_test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import unittest
+
+class ProjectRunning(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_is_server_running(self):
+        self.browser.get('http://127.0.0.1:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.Main()
